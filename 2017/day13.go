1fc1a51ab4355d35f2ccc8ed4de6aa24af1751d8
Liz Fong-Jones <elizabeth@ugcs.caltech.edu>
2017-12-22 22:12:33 -0500

    add day13b solution.

--- 2017/day13.go
+++ 2017/day13.go
@@ -17,7 +17,8 @@ type Scanner struct {
 }
 type Board struct {
 	PacketDepth int
-	Scanners    map[int]*Scanner
+	MaxDepth    int
+	Scanners    map[int]Scanner
 }
 
 func main() {
@@ -28,10 +29,43 @@ func main() {
 		fmt.Printf("Could not open file %s because %v.\n", *inputFile, err)
 	}
 	contents := string(bytes)
-	lines := strings.Split(contents, "\n")
-	b := Board{-2, make(map[int]*Scanner)}
 
-	maxDepth := 0
+	b := SetupBoard(contents)
+
+	b = b.Enter()
+	totalSeverity := 0
+	for b.PacketDepth <= b.MaxDepth {
+		severity := 0
+		b, _, severity = b.Tick()
+		totalSeverity += severity
+	}
+	fmt.Printf("Total severity on naive entry: %d\n", totalSeverity)
+
+	// Ensure we don't have to replay the state up to the tick before we entered every time.
+	memo := SetupBoard(contents)
+
+	outer:
+	for toDelay := 1;; toDelay++ {
+		memo, _, _ = memo.Tick()
+
+		b := memo.Enter()
+		for b.PacketDepth <= b.MaxDepth {
+			caught := false
+			b, caught, _ = b.Tick()
+			if caught {
+				// We were caught; this delay won't work.
+				continue outer
+			}
+		}
+		fmt.Printf("Got through with delay %d.\n", toDelay)
+		break
+	}
+}
+
+func SetupBoard(contents string) Board {
+	lines := strings.Split(contents, "\n")
+	b := Board{-2, 0, make(map[int]Scanner)}
+
 	for _, l := range lines {
 		if len(l) == 0 {
 			break
@@ -41,42 +75,47 @@ func main() {
 		d, err := strconv.Atoi(parts[0])
 		if err != nil {
 			fmt.Printf("Failed to parse depth in '%s'\n", l)
-			return
+			return b
 		}
 
 		r, err := strconv.Atoi(parts[1])
 		if err != nil {
 			fmt.Printf("Failed to parse range in '%s'\n", l)
-			return
+			return b
 		}
-		b.Scanners[d] = &Scanner{0, r, true}
-		if d > maxDepth {
-			maxDepth = d
+		b.Scanners[d] = Scanner{0, r, true}
+		if d > b.MaxDepth {
+			b.MaxDepth = d
 		}
 	}
-	b.Enter()
-	totalSeverity := 0
-	for i := 0; i <= maxDepth; i++ {
-		totalSeverity += b.Tick()
+	return b
+}
+
+func (b Board) Enter() Board {
+	ret := b
+	ret.Scanners = make(map[int]Scanner)
+	for k,v := range b.Scanners {
+		ret.Scanners[k] = v
 	}
-	fmt.Printf("Total severity: %d\n", totalSeverity)
+	ret.PacketDepth = -1
+	return ret
 }
 
-func (b *Board) Enter() {
-	b.PacketDepth = -1
-}
+func (b Board) Tick() (Board, bool, int) {
+	ret := b
 
-func (b *Board) Tick() int {
+	caught := false
 	severity := 0
-	if b.PacketDepth >= -1 {
-		b.PacketDepth++
+	if ret.PacketDepth >= -1 {
+		ret.PacketDepth++
 	}
-	if s, found := b.Scanners[b.PacketDepth]; found {
+	if s, found := ret.Scanners[ret.PacketDepth]; found {
 		if s.Pos == 0 {
-			severity = b.PacketDepth * s.Range
+			caught = true
+			severity = ret.PacketDepth * s.Range
 		}
 	}
-	for _, v := range b.Scanners {
+	for k, v := range ret.Scanners {
 		if v.MovingDown {
 			v.Pos++
 			if v.Pos == v.Range-1 {
@@ -88,6 +127,7 @@ func (b *Board) Tick() int {
 				v.MovingDown = true
 			}
 		}
+		ret.Scanners[k] = v
 	}
-	return severity
+	return ret, caught, severity
 }
