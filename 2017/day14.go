494cbf0b879a055097e1c3e9b113155f2b2b24c5
Liz Fong-Jones <elizabeth@ugcs.caltech.edu>
2017-12-22 23:13:27 -0500

    add day14b solution.

--- 2017/day14.go
+++ 2017/day14.go
@@ -21,6 +21,38 @@ func main() {
 	}
 
 	fmt.Printf("Found %d set bits.\n", set)
+
+	var addedToRegions [128][128]bool
+
+	regions := 0
+	for i := 0; i < 128; i++ {
+		for j := 0; j < 128; j++ {
+			if addedToRegions[i][j] || !bitField[i][j] {
+				// Only consider as seeds set tiles that are not part of regions.
+				continue
+			}
+			regions++
+			expandRegion(i, j, &bitField, &addedToRegions)
+		}
+	}
+	fmt.Printf("Found %d non-overlapping regions.\n", regions)
+}
+
+func expandRegion(x, y int, bits, used *[128][128]bool) {
+	used[x][y] = true
+	// Try to expand up, down, left, and right.
+	if x > 0 && bits[x-1][y] && !used[x-1][y] {
+		expandRegion(x-1, y, bits, used)
+	}
+	if x < 127 && bits[x+1][y] && !used[x+1][y] {
+		expandRegion(x+1, y, bits, used)
+	}
+	if y > 0 && bits[x][y-1] && !used[x][y-1] {
+		expandRegion(x, y-1, bits, used)
+	}
+	if y < 127 && bits[x][y+1] && !used[x][y+1] {
+		expandRegion(x, y+1, bits, used)
+	}
 }
 
 func toBits(d []int) [128]bool {
