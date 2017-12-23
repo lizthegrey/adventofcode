38d7e234b7e8056f05627e20d70641d362f4499a
Liz Fong-Jones <elizabeth@ugcs.caltech.edu>
2017-12-23 17:02:51 -0500

    add day20b solution.

--- 2017/day20.go
+++ 2017/day20.go
@@ -11,13 +11,15 @@ import (
 )
 
 var inputFile = flag.String("inputFile", "inputs/day20.input", "Relative file path to use as input.")
+var cycles = flag.Int("cycles", 1000, "The number of cycles to simulate.")
 
 type Coord struct {
 	X, Y, Z int
 }
 
 type Particle struct {
-	P, V, A Coord
+	P, V, A   Coord
+	Destroyed bool
 }
 
 type Board []*Particle
@@ -45,7 +47,7 @@ func main() {
 			v, _ := strconv.Atoi(s[i])
 			r[i-1] = v
 		}
-		p := Particle{Coord{r[0], r[1], r[2]}, Coord{r[3], r[4], r[5]}, Coord{r[6], r[7], r[8]}}
+		p := Particle{Coord{r[0], r[1], r[2]}, Coord{r[3], r[4], r[5]}, Coord{r[6], r[7], r[8]}, false}
 		particles[i] = &p
 	}
 
@@ -59,12 +61,47 @@ func main() {
 		}
 	}
 	fmt.Printf("Particle with smallest acceleration has index %d\n", smallestAccelId)
+
+	for i := 0; i < *cycles; i++ {
+		particles.Tick()
+	}
+	remaining := 0
+	for _, p := range particles {
+		if !p.Destroyed {
+			remaining++
+		}
+	}
+	fmt.Printf("After %d cycles, %d particles remain.\n", *cycles, remaining)
 }
 
 func (b Board) Tick() {
-	for _, p := range b {
+	// First, update positions of all the particles
+	// Mark locations that are occupied by at least one particle with the index.
+	// Mark particles as to be destroyed if the index doesn't match their own index.
+	// (including marking the other particle at that location to destroy as well).
+	// Actually destroy each particle that is marked.
+
+	occupied := make(map[Coord]int)
+
+	for i, p := range b {
+		if p.Destroyed {
+			continue
+		}
 		p.V = p.V.Add(p.A)
 		p.P = p.P.Add(p.V)
+		occupied[p.P] = i
+	}
+
+	toDestroy := make(map[int]bool)
+	for i, p := range b {
+		if other, collision := occupied[p.P]; collision && other != i {
+			toDestroy[other] = true
+			toDestroy[i] = true
+		}
+	}
+
+	for i := range toDestroy {
+		b[i].Destroyed = true
 	}
 }
 
