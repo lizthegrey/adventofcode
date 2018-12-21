b6c5e2a3b80b556d158fbd827d8a91a0cc0dfc92
Liz Fong-Jones <elizabeth@ugcs.caltech.edu>
2018-12-20 20:15:19 -0500

    Add working day20ab solution.

--- 2018/day20.go
+++ 2018/day20.go
@@ -5,25 +5,27 @@ import (
 	"flag"
 	"fmt"
 	"os"
-	"regexp"
 )
 
 var inputFile = flag.String("inputFile", "inputs/day20.input", "Relative file path to use as input.")
-var verbose = flag.Bool("verbose", false, "Whether to print verbose debug output.")
-var partB = flag.Bool("partB", false, "Whether to use the Part B logic.")
+var minDoors = flag.Int("minDoors", 1000, "The minimum number of doors to count a room.")
 
 type Coord struct {
 	X, Y int
 }
 
+// Track for each item in the queue, how far the room is, so that we can
+// track the distance for its children.
 type BFSEntry struct {
 	c Coord
 	d int
 }
 
 // Coord -> NESW; missing adjacency = impassable.
+// Less memory-efficient than an integer, but easier cognitively.
 type Adjacencies map[Coord]string
 
+// Function Add creates a door between a coordinate and the room in direction d from it.
 func (a Adjacencies) Add(l Coord, d rune) {
 	found := false
 	for _, c := range a[l] {
@@ -33,11 +35,13 @@ func (a Adjacencies) Add(l Coord, d rune) {
 			break
 		}
 	}
+	// Don't add the same room link twice.
 	if !found {
 		a[l] += string(d)
 	}
 }
 
+// Function Neighbors returns a slice of coordinates of valid neighboring rooms.
 func (a Adjacencies) Neighbors(c Coord) []Coord {
 	ret := make([]Coord, 0)
 	for _, d := range a[c] {
@@ -57,6 +61,27 @@ func (a Adjacencies) Neighbors(c Coord) []Coord {
 	return ret
 }
 
+// Function Move creates a hole in the wall in both directions and returns the new coordinate.
+func (l Coord) Move(adj Adjacencies, d rune) Coord {
+	adj.Add(l, d)
+	switch d {
+	case 'N':
+		l.Y--
+		adj.Add(l, 'S')
+	case 'E':
+		l.X++
+		adj.Add(l, 'W')
+	case 'S':
+		l.Y++
+		adj.Add(l, 'N')
+	case 'W':
+		l.X--
+		adj.Add(l, 'E')
+	}
+	return l
+}
+
+// Parse tree object tracking sibling and branch children for evaluation.
 type PathSubstring struct {
 	Payload     string
 	Branches    []*PathSubstring // OR group underneath us.
@@ -64,31 +89,41 @@ type PathSubstring struct {
 	Parent      *PathSubstring   // Pointer to node above. null if we're the root.
 }
 
-func (p *PathSubstring) GeneratePaths() []string {
-	var branches []string
+// Takes in a starting coordinatee, and punches holes starting from there that match the pattern.
+func (p *PathSubstring) GeneratePaths(adj Adjacencies, l Coord) map[Coord]bool {
+	// Do the unconditional moves from our payload.
+	for _, c := range p.Payload {
+		// Modifies l as we move along.
+		l = l.Move(adj, c)
+	}
+	// Now we're at the end of our initial movement.
 
-	// Deal with any sub-branches
-	if len(p.Branches) != 0 {
+	// Deal with any sub-branches, starting from the position after initial movement.
+	postBranchCoords := make(map[Coord]bool)
+	if len(p.Branches) > 0 {
 		for _, b := range p.Branches {
-			branches = append(branches, b.GeneratePaths()...)
+			for k := range b.GeneratePaths(adj, l) {
+				postBranchCoords[k] = true
+			}
 		}
 	} else {
-		branches = append(branches, "")
+		// If no sub-branches, just pass the initial movement coordinate.
+		postBranchCoords[l] = true
 	}
 
-	// Deal with any siblings.
-	suffixes := []string{""}
-	if p.NextSibling != nil {
-		suffixes = p.NextSibling.GeneratePaths()
+	// If no siblings, return where we wound up after initial and branches.
+	if p.NextSibling == nil {
+		return postBranchCoords
 	}
 
-	results := make([]string, 0)
-	for _, br := range branches {
-		for _, post := range suffixes {
-			results = append(results, p.Payload+br+post)
+	// Starting from each branch point, also check the result after siblings.
+	finalCoords := make(map[Coord]bool)
+	for b := range postBranchCoords {
+		for k := range p.NextSibling.GeneratePaths(adj, b) {
+			finalCoords[k] = true
 		}
 	}
-	return results
+	return finalCoords
 }
 
 func main() {
@@ -99,7 +134,6 @@ func main() {
 	}
 	defer f.Close()
 
-	var r *regexp.Regexp
 	var raw string
 
 	reader := bufio.NewReader(f)
@@ -109,8 +143,6 @@ func main() {
 			break
 		}
 		l = l[:len(l)-1]
-		r = regexp.MustCompile(l)
-		// Trim ^ and $
 		raw = l[1 : len(l)-1]
 	}
 
@@ -118,10 +150,6 @@ func main() {
 	current := &PathSubstring{}
 	root := current
 	for _, c := range raw {
-		if *verbose {
-			fmt.Printf("%c, %v\n", c, current)
-		}
-
 		switch c {
 		case 'N':
 			fallthrough
@@ -130,85 +158,69 @@ func main() {
 		case 'S':
 			fallthrough
 		case 'W':
+			// We're adding literals to a block.
 			current.Payload += string(c)
 		case '(':
+			// Start a new child and put it under the current node.
 			child := &PathSubstring{}
 			child.Parent = current
 			current.Branches = []*PathSubstring{child}
 			current = child
 		case ')':
+			// Close off the current block, and then create a sibling to our parent.
 			sibling := &PathSubstring{}
 			sibling.Parent = current.Parent.Parent
 			current.Parent.NextSibling = sibling
 			current = sibling
 		case '|':
-			sibling := &PathSubstring{}
-			sibling.Parent = current.Parent
-			current.Parent.Branches = append(current.Parent.Branches, sibling)
-			current = sibling
+			// Create a branch alternative.
+			alternative := &PathSubstring{}
+			alternative.Parent = current.Parent
+			current.Parent.Branches = append(current.Parent.Branches, alternative)
+			current = alternative
 		}
 	}
 
-	paths := root.GeneratePaths()
-
-	for _, p := range paths {
-		// Verify that each path matches the regexp as a final test.
-		if !r.MatchString(p) {
-			fmt.Printf("Failed to match generated route %s.\n", p)
-			return
-		}
-		if *verbose {
-			fmt.Println(p)
-		}
-	}
-
+	// Track the holes we've madde in walls.
 	adj := make(Adjacencies)
-	for _, p := range paths {
-		l := Coord{0, 0}
-		// Walk through the path, tearing down walls and putting in doors as we go.
-		for _, d := range p {
-			adj.Add(l, d)
-			switch d {
-			case 'N':
-				l.Y--
-				adj.Add(l, 'S')
-			case 'E':
-				l.X++
-				adj.Add(l, 'W')
-			case 'S':
-				l.Y++
-				adj.Add(l, 'N')
-			case 'W':
-				l.X--
-				adj.Add(l, 'E')
-			}
-		}
-	}
 
-	// Start our breadth first search.
+	// Start from 0,0 and pass the head of the parse tree.
+	root.GeneratePaths(adj, Coord{0, 0})
+
+	// Start our breadth first search for distances.
 	seenShortestDistances := make(map[Coord]int)
 	longest := 0
 	q := []BFSEntry{{Coord{0, 0}, 0}}
+
+	// Part B asks us how many long paths there are.
+	longPaths := 0
 
 	for len(q) > 0 {
 		l := q[0]
 		q = q[1:]
 
 		if _, ok := seenShortestDistances[l.c]; ok {
+			// Termination condition: don't add to the queue if we've seen a node.
 			continue
 		}
 
 		neighbors := adj.Neighbors(l.c)
 		qadditions := make([]BFSEntry, len(neighbors))
 		seenShortestDistances[l.c] = l.d
+
+		if l.d >= *minDoors {
+			longPaths++
+		}
 		if l.d > longest {
 			longest = l.d
 		}
 
 		for i, n := range neighbors {
+			// Remember to increment the distance for our neighbors.
 			qadditions[i] = BFSEntry{n, l.d + 1}
 		}
 		q = append(q, qadditions...)
 	}
-	fmt.Println(longest)
+
+	fmt.Printf("Furthest room %d away; %d rooms %d doors away or more.\n", longest, longPaths, *minDoors)
 }
