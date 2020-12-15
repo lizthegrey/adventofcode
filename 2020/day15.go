90d1728c768286be7f10c6fac795e747522d8ede
Liz Fong-Jones <lizf@honeycomb.io>
2020-12-14 21:50:10 -0800

    clean up init code

--- 2020/day15.go
+++ 2020/day15.go
@@ -22,31 +22,27 @@ func main() {
 	split := strings.Split(contents, "\n")
 	split = split[:len(split)-1]
 	split = strings.Split(split[0], ",")
-	init := make([]int, len(split))
+	seen := make(map[int]int)
+	prev := -1
 	for i, s := range split {
 		n, err := strconv.Atoi(s)
 		if err != nil {
 			fmt.Printf("Failed to parse %s\n", s)
 			break
 		}
-		init[i] = n
+		seen[n] = i
+		prev = n
 	}
-	seen := make(map[int]int)
-	prev := -1
 	max := 2020
 	if *partB {
 		max = 30000000
 	}
-	for i := 0; i < max; i++ {
+	for i := len(split); i < max; i++ {
 		var val int
-		if i < len(init) {
-			val = init[i]
+		if prevPos, seen := seen[prev]; seen {
+			val = (i - 1) - prevPos
 		} else {
-			if prevPos, seen := seen[prev]; seen {
-				val = (i - 1) - prevPos
-			} else {
-				val = 0
-			}
+			val = 0
 		}
 		if *debug {
 			fmt.Println(val)
