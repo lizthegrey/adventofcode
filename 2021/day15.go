b235caeab624560517ded8f8b3fad95841e3cb9f
Liz Fong-Jones <lizf@honeycomb.io>
2021-12-14 22:44:36 -0800

    use HeapQueue

--- 2021/day15.go
+++ 2021/day15.go
@@ -1,11 +1,11 @@
 package main
 
 import (
+	"container/heap"
 	"context"
 	"flag"
 	"fmt"
 	"io/ioutil"
-	"math"
 	"strings"
 
 	"github.com/lizthegrey/adventofcode/2021/trace"
@@ -20,6 +20,39 @@ var tr = otel.Tracer("day15")
 type RiskMap map[Coord]int
 type Coord struct {
 	R, C int
+}
+type HeapQueue struct {
+	Elems     *[]Coord
+	Score     RiskMap
+	Positions map[Coord]int
+}
+
+func (h HeapQueue) Len() int           { return len(*h.Elems) }
+func (h HeapQueue) Less(i, j int) bool { return h.Score[(*h.Elems)[i]] < h.Score[(*h.Elems)[j]] }
+func (h HeapQueue) Swap(i, j int) {
+	h.Positions[(*h.Elems)[i]], h.Positions[(*h.Elems)[j]] = h.Positions[(*h.Elems)[j]], h.Positions[(*h.Elems)[i]]
+	(*h.Elems)[i], (*h.Elems)[j] = (*h.Elems)[j], (*h.Elems)[i]
+}
+
+func (h HeapQueue) Push(x interface{}) {
+	h.Positions[x.(Coord)] = len(*h.Elems)
+	*h.Elems = append(*h.Elems, x.(Coord))
+}
+
+func (h HeapQueue) Pop() interface{} {
+	old := *h.Elems
+	n := len(old)
+	x := old[n-1]
+	*h.Elems = old[0 : n-1]
+	delete(h.Positions, x)
+	return x
+}
+
+func (h HeapQueue) Position(x Coord) int {
+	if pos, ok := h.Positions[x]; ok {
+		return pos
+	}
+	return -1
 }
 
 func main() {
@@ -71,24 +104,13 @@ func AStar(r RiskMap, src, dst *Coord) int {
 	fScore := RiskMap{
 		*src: src.Heuristic(dst),
 	}
-	workList := map[Coord]bool{
-		*src: true,
-	}
+	workList := HeapQueue{&[]Coord{*src}, fScore, make(RiskMap)}
+	heap.Init(&workList)
 	history := make(map[Coord]Coord)
 
-	for len(workList) != 0 {
+	for len(*workList.Elems) != 0 {
 		// Pop the current node off the worklist.
-		currentScore := math.MaxInt
-		var current Coord
-		for v := range workList {
-			if score, ok := fScore[v]; ok {
-				if score < currentScore {
-					current = v
-					currentScore = score
-				}
-			}
-		}
-		delete(workList, current)
+		current := heap.Pop(&workList).(Coord)
 
 		if current == *dst {
 			// Reconstruct the score by retracing our path to start.
@@ -105,8 +127,10 @@ func AStar(r RiskMap, src, dst *Coord) int {
 				history[n] = current
 				gScore[n] = proposedScore
 				fScore[n] = proposedScore + n.Heuristic(dst)
-				if !workList[n] {
-					workList[n] = true
+				if pos := workList.Position(n); pos == -1 {
+					heap.Push(&workList, n)
+				} else {
+					heap.Fix(&workList, pos)
 				}
 			}
 		}
