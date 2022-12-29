d7a453239707182702fc28627e11fbc2576c5be8
Liz Fong-Jones <lizf@honeycomb.io>
2022-12-30 07:29:22 +1100

    add provisional day21a solution

--- 2022/day21.go
+++ 2022/day21.go
@@ -10,23 +10,10 @@ import (
 
 var inputFile = flag.String("inputFile", "inputs/day21.input", "Relative file path to use as input.")
 
-type channels map[string]chan int
-
-func (c channels) get(key string) chan int {
-	if ret, ok := c[key]; ok {
-		return ret
-	} else {
-		ret = make(chan int, 100)
-		c[key] = ret
-		return ret
-	}
-}
-
-func (c channels) start(key string, f func() int) {
-	v := f()
-	for i := 0; i < 100; i++ {
-		c.get(key) <- v
-	}
+type monkey struct {
+	cached  int
+	compute func() int
+	pending int
 }
 
 func main() {
@@ -39,33 +26,26 @@ func main() {
 	split := strings.Split(contents, "\n")
 
 	// part A
-	// Cheat using goroutines.
-	monkeys := make(channels)
+	monkeys := make(map[string]*monkey)
+	deps := make(map[string][]string)
 	for _, s := range split[:len(split)-1] {
 		parts := strings.Split(s, ": ")
 		key := parts[0]
 
-		// Pre-populate the map
-		monkeys.get(key)
-
 		command := strings.Split(parts[1], " ")
 		if len(command) == 1 {
 			val, _ := strconv.Atoi(command[0])
-			go monkeys.start(key, func() int {
-				return val
-			})
+			monkeys[key] = &monkey{compute: func() int { return val }}
 		} else {
 			x := command[0]
 			op := command[1]
 			y := command[2]
 
-			// Pre-populate the map
-			monkeys.get(x)
-			monkeys.get(y)
-
-			go monkeys.start(key, func() int {
-				a := <-monkeys.get(x)
-				b := <-monkeys.get(y)
+			deps[x] = append(deps[x], key)
+			deps[y] = append(deps[y], key)
+			monkeys[key] = &monkey{pending: 2, compute: func() int {
+				a := monkeys[x].cached
+				b := monkeys[y].cached
 				switch op {
 				case "+":
 					return a + b
@@ -77,8 +57,22 @@ func main() {
 					return a / b
 				}
 				return -1
-			})
+			}}
 		}
 	}
-	fmt.Println(<-monkeys["root"])
+
+	for monkeys["root"].pending != -1 {
+		for k, m := range monkeys {
+			if m.pending != 0 {
+				continue
+			}
+			// This node has newly become unblocked.
+			m.pending -= 1
+			m.cached = m.compute()
+			for _, dep := range deps[k] {
+				monkeys[dep].pending -= 1
+			}
+		}
+	}
+	fmt.Println(monkeys["root"].cached)
 }
