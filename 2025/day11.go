d8637154f5f9168e1287ffcf4ba3fc3c69d7873b
Liz Fong-Jones <lizf@honeycomb.io>
2025-12-11 18:53:49 +1100

    switch to using a bitset

--- 2025/day11.go
+++ 2025/day11.go
@@ -4,11 +4,41 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
-	"slices"
 	"strings"
 )
 
 var inputFile = flag.String("inputFile", "inputs/day11.input", "Relative file path to use as input.")
+
+// for some reason coincidentally there are 576 different devices, which is 64*9.
+// this cannot possibly be a coincidence.
+type CacheKey [9]uint64
+
+func (c CacheKey) Used(d Device) bool {
+	for i := range c {
+		if d.Key[i]&c[i] > 0 {
+			return true
+		}
+	}
+	return false
+}
+
+func (c CacheKey) MarkUsed(d Device) CacheKey {
+	for i := range c {
+		c[i] |= d.Key[i]
+	}
+	return c
+}
+
+type Device struct {
+	Name  string
+	Conns []string
+	Key   CacheKey
+}
+
+type State struct {
+	Visited CacheKey
+	Cur     string
+}
 
 func main() {
 	flag.Parse()
@@ -19,38 +49,46 @@ func main() {
 	contents := string(bytes)
 	split := strings.Split(contents, "\n")
 
-	gates := make(map[string][]string)
-	for _, s := range split[:len(split)-1] {
+	devices := make(map[string]Device)
+	for i, s := range split[:len(split)-1] {
 		parts := strings.Split(s, ": ")
-		in := parts[0]
-		var out []string
+
+		var d Device
+		d.Name = parts[0]
 		for _, v := range strings.Split(parts[1], " ") {
-			out = append(out, v)
+			d.Conns = append(d.Conns, v)
 		}
-		gates[in] = out
+		d.Key[i/64] = 1 << (i % 64)
+		devices[d.Name] = d
 	}
-	fmt.Println(compute(gates, []string{"you"}, "out"))
-	var countB uint64
-	countB += compute(gates, []string{"svr"}, "dac") * compute(gates, []string{"svr", "dac"}, "fft") * compute(gates, []string{"svr", "dac", "fft"}, "out")
-	countB += compute(gates, []string{"svr"}, "fft") * compute(gates, []string{"svr", "fft"}, "dac") * compute(gates, []string{"svr", "fft", "dac"}, "out")
-	fmt.Println(countB)
+	memo := make(map[State]uint64)
+	fmt.Println(compute(devices, memo, State{devices["you"].Key, "you"}, "out"))
+	clear(memo)
+	fmt.Println(compute(devices, memo, State{devices["svr"].Key, "svr"}, "out"))
 }
 
-func compute(gates map[string][]string, visited []string, fin string) uint64 {
-	cur := visited[len(visited)-1]
-	if cur == fin {
+func compute(devices map[string]Device, memo map[State]uint64, state State, fin string) uint64 {
+	var ret uint64
+	if state.Cur == fin {
+		// Mark invalid entries bad
+		if state.Visited.Used(devices["svr"]) && !(state.Visited.Used(devices["fft"]) && state.Visited.Used(devices["dac"])) {
+			return 0
+		}
 		return 1
 	}
-	var ret uint64
-	visited = slices.Grow(visited, 1)
-	for _, v := range gates[cur] {
-		if slices.Contains(visited, v) {
+	if ret, ok := memo[state]; ok {
+		return ret
+	}
+	for _, v := range devices[state.Cur].Conns {
+		if state.Visited.Used(devices[v]) {
 			continue
 		}
-		visited = visited[:len(visited)+1]
-		visited[len(visited)-1] = v
-		ret += compute(gates, visited, fin)
-		visited = visited[:len(visited)-1]
+		next := state
+		next.Cur = v
+		next.Visited = next.Visited.MarkUsed(devices[v])
+		ret += compute(devices, memo, next, fin)
 	}
+
+	memo[state] = ret
 	return ret
 }
